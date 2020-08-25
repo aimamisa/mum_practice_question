1beb3bdefe6e5f5f1c110b3a6544d537c4f3ac0f
aimamisa <imamabdulrazaq@gmail.com>
2020-08-25 11:53:10 +0100

    Added Questions & Answers: 1. isCentered Function 2. isDual Function 3. isAllPossibilities Function

--- Questions/Answers/IsCentered_Function/JAVA/IsCentered_Function.java
+++ Questions/Answers/IsCentered_Function/JAVA/IsCentered_Function.java
@@ -1,24 +1,34 @@
-public int isCentered(int[] a) {
-    //array should contains odd numbers of elements.
-    if(a.Length % 2 != 0) {
-        for (int i = 0; i < a.Length; i++){
-            //only one element is centred itself.
-            if (a.Length == 1)
-                break;
+public class Centered {
 
-            //finding the value of central element
-            centerVal = a[a.Length / 2];
+    public static void main(String[] args) {
+        int[] a = {};
+        int i = isCentered(a);
 
-            if(centerVal >= a[i])
-            {
-                sum = 0;
-                break;
+        System.out.println(i);
+    }
+	
+    public static int isCentered(int[] a) {
+        //array should contains odd numbers of elements.
+        if(a.length % 2 != 0) {
+            
+            for (int i = 0; i < a.length; i++){
+                //only one element is centred itself.
+                if (a.length == 1)
+                    break;
+    
+                //finding the value of central element
+                centerVal = a[a.length / 2];
+    
+                if(centerVal >= a[i]){
+                    sum = 0;
+                    break;
+                }
             }
         }
+        else {
+            sum = 0;
+        }
+    
+        return rtnval;
     }
-    else {
-        sum = 0;
-    }
-
-    return rtnval;
 }
--- Questions/Answers/isDual_Function/JAVA/isDual_Function.java
+++ Questions/Answers/isDual_Function/JAVA/isDual_Function.java
@@ -1,21 +1,31 @@
-public int isDual(int[] a) {
-    int rtnval = 1;
+public class Dual {
 
-    if (a.Length > 0) {
+    public static void main(String[] args) {
+        int[] a = {};
+        int i = isDual(a);
 
-        //should be even
-        if (a.Length % 2 == 0) {
-            for (int i = 2; i < a.Length; i = i + 2) {
-                if (a[0] + a[1] != a[i] + a[i + 1]) {
-                    rtnval = 0;
-                    break;
+        System.out.println(i);
+    }
+	
+    public static int isDual(int[] a) {
+        int rtnval = 1;
+    
+        if (a.length > 0) {
+    
+            //should be even
+            if (a.length % 2 == 0) {
+                for (int i = 2; i < a.length; i = i + 2) {
+                    if (a[0] + a[1] != a[i] + a[i + 1]) {
+                        rtnval = 0;
+                        break;
+                    }
                 }
             }
+            else {
+                rtnval = 0;
+            }
         }
-        else {
-            rtnval = 0;
-        }
+    
+        return rtnval;
     }
-
-    return rtnval;
 }
